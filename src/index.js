f5ef3517a0752df28f820305d718b1341f05e313
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-14 23:22:01 +0200

    feat: add todo-list's components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header';
+import ToDoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
+import ItemStatusFilter from './components/item-status-filter';
 
+import './index.css';
+
+const App = () => {
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 0 },
+    { label: 'Learn React', important: false, id: 1 },
+    { label: 'Build Awesome App', important: true, id: 2 },
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+      <ToDoList todos={todoData} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
